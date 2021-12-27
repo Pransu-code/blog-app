3243c952cc5e896993dd2c765f7017751603c7dc
PRANSU YADAV <71815813+Pransu-code@users.noreply.github.com>
2021-12-28 00:44:08 +0545

    blog app post

---
+++ Linkedin_post/blog-app/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Main from "./Main.js";
+
+import Home from "./Home";
+
+function App() {
+  return ( 
+    <Main />
+  );
+}
+
+export default App;
